1c429238920faa24d1c6b6014865369f295000ce
YakovlIlya228 <ilya.coding@yandex.ru>
2020-08-21 07:16:37 +0300

    build.gradle redefined, base url moved to build config

--- app/src/main/java/com/example/testtask/Network/RetrofitService.kt
+++ app/src/main/java/com/example/testtask/Network/RetrofitService.kt
@@ -1,14 +1,14 @@
 package com.example.testtask.Network
 
+import com.example.testtask.BuildConfig
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitService {
-    val BASE_URL = "https://reqres.in"
 
     fun getRetrofit(): CallAdapter {
         return Retrofit.Builder()
-            .baseUrl(BASE_URL)
+            .baseUrl(BuildConfig.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(CallAdapter::class.java)
